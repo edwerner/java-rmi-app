43662c74f360dffc215b654cda4b9d889953ef7b
Ed Werner <android.devops@gmail.com>
2018-10-10 17:39:15 -0400

    adds heartbeat polling

--- Client.java
+++ Client.java
@@ -2,21 +2,35 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer; 
+import java.util.TimerTask;
 
 public class Client {
-private static int TIME = 999999999; 
 private SimpleDateFormat simpleDateFormat;
-    private Client() {} 
+
+    private static boolean running;
+
+    private Client() {
+
+    }
+
     public static void main(String[] args) {
         try {  
             // Getting the registry 
-            Registry registry = LocateRegistry.getRegistry(null); 
+            Registry registry = LocateRegistry.getRegistry(null);
+            String date = ""; 
 
             // Looking up the registry for the remote object 
             Heartbeat stub = (Heartbeat) registry.lookup("Heartbeat");
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
-            String date = simpleDateFormat.format(new Date());
-            stub.printMsg(date);
+            running = true;
+
+            while (running) {
+                date = simpleDateFormat.format(new Date());
+                stub.printMsg(date);
+                Thread.sleep(5000);
+            }
+
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString()); 
             e.printStackTrace(); 
