3d066ab91e2b4774e66dd3f44390319cab1166b7
Ed Werner <android.devops@gmail.com>
2018-10-12 12:55:04 -0400

    removes surrounding try-catch

--- Receiver.java
+++ Receiver.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.rmi.ConnectException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry; 
+import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,53 +14,49 @@ public class Receiver {
     private Receiver() {}
 
     public static void main(String[] args) throws RemoteException, NotBoundException, InterruptedException {
-        try {  
-            // Getting the registry 
-            Registry registry = LocateRegistry.getRegistry(null);
-            String date; 
-            //int failureCounter = 5;
-            int failureCounter = randInt(3,5);
 
-            // Looking up the registry for the remote object 
-            Heartbeat heartbeat = (Heartbeat) registry.lookup("Heartbeat");
-            Heartbeat redundancy = (Heartbeat) registry.lookup("Heartbeat");
+        // Getting the registry 
+        Registry registry = LocateRegistry.getRegistry(null);
+        String date;
+        //int failureCounter = 5;
+        int failureCounter = randInt(3, 5);
 
-            // time-date stamp
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
+        // Looking up the registry for the remote object 
+        Heartbeat heartbeat = (Heartbeat) registry.lookup("Heartbeat");
+        Heartbeat redundancy = (Heartbeat) registry.lookup("Heartbeat");
 
-            // heartbeat is running
-            running = true;
-            
-            // get Java runtime
-            Runtime rt = Runtime.getRuntime();
+        // time-date stamp
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
 
-            while (running == true) {
-              if (failureCounter > 0) {
-                 date = simpleDateFormat.format(new Date());
-                 heartbeat.printMsg(date);
-                 Thread.sleep(5000);
-                 failureCounter--;
-              } else {
+        // heartbeat is running
+        running = true;
+
+        // get Java runtime
+        Runtime rt = Runtime.getRuntime();
+
+        while (running == true) {
+            if (failureCounter > 0) {
+                date = simpleDateFormat.format(new Date());
+                heartbeat.printMsg(date);
+                Thread.sleep(5000);
+                failureCounter--;
+            } else {
                 running = false;
                 try {
-                  redundancy.printMsg("Hearbeat has crashed");
-                  rt.exec("java Receiver");
-                  running = true;
-                  failureCounter = 5;
+                    redundancy.printMsg("Hearbeat has crashed");
+                    rt.exec("java Receiver");
+                    running = true;
+                    failureCounter = 5;
                 } catch (IOException e) {
-                  e.printStackTrace();
-                } 
+                    e.printStackTrace();
+                }
                 redundancy.printMsg("Hearbeat has recovered");
                 throw new ConnectException("Heartbeat receiver has disconnected");
-              }
-          }
-        } catch (ConnectException e) {
-            System.err.println("Client exception: " + e.toString()); 
-            e.printStackTrace();
-        } 
-    } 
-    
-    public static int randInt(int min, int max) {   	
+            }
+        }
+    }
+
+    public static int randInt(int min, int max) {
         Random rand = new Random();
         int randomNum = rand.nextInt((max - min) + 1) + min;
         return randomNum;
